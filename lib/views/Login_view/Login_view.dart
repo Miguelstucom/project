b87e33744c2e471fec4a13bc08a791ca9b12a02f
Miguel <miguel.stucom@gmail.com>
2024-06-13 01:01:14 +0200

    LOGIN

--- lib/views/Login_view/Login_view.dart
+++ lib/views/Login_view/Login_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class Login extends StatefulWidget {
   final String title = "Login Page";
 
@@ -13,62 +12,98 @@ class Login extends StatefulWidget {
 
 class _MyHomePageState extends State<Login> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  FocusNode nodeEmail = FocusNode();
+  FocusNode nodePass = FocusNode();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nodeEmail.addListener(_updateState);
+    nodePass.addListener(_updateState);
+  }
+
+  @override
+  void dispose() {
+    nodeEmail.removeListener(_updateState);
+    nodePass.removeListener(_updateState);
+    nodeEmail.dispose();
+    nodePass.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _updateState() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/logo_full_512.png',height: 250,),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Tienes algo que compartir?",
-                        style: GoogleFonts.nunitoSans(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Colors.black,),
-                        textAlign: TextAlign.left,
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        body: Form(
+          key: _formKey,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!nodeEmail.hasFocus && !nodePass.hasFocus)
+                    Image.asset(
+                      'assets/images/logo.webp',
+                      height: 250,
+                    ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Tienes algo que compartir?",
+                      style: GoogleFonts.nunitoSans(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                        color: Colors.black,
                       ),
+                      textAlign: TextAlign.left,
                     ),
-                    const SizedBox(
-                      height: 10,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Construye tu futuro junto a un profesional",
+                      style: GoogleFonts.nunitoSans(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.black),
+                      textAlign: TextAlign.left,
                     ),
-                     Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "Construye tu futuro con un profesional",
-                        style: GoogleFonts.nunitoSans(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.black),
-                        textAlign: TextAlign.left,
-                      ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
+                    child: TextFormField(
                       controller: _usernameController,
+                      focusNode: nodeEmail,
                       decoration: const InputDecoration(
                           labelText: 'Email',
-                          labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
+                          filled: true,
+                          hintText: 'ejemplo@ejemplo.es',
+                          fillColor: Colors.white,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                          border: InputBorder.none,
                           prefixIcon: Icon(Icons.person)),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -77,19 +112,26 @@ class _MyHomePageState extends State<Login> {
                         return null;
                       },
                     ),
-                    const SizedBox(height: 16.0),
-                    TextFormField(
+                  ),
+                  const SizedBox(height: 20.0),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextFormField(
                       controller: _passwordController,
                       obscureText: true,
+                      focusNode: nodePass,
                       decoration: const InputDecoration(
                           labelText: 'Contraseña',
-                          labelStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                          ),
+                          filled: true,
+                          hintText: 'Contraseña',
+                          fillColor: Colors.white,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                          border: InputBorder.none,
                           prefixIcon: Icon(Icons.lock)),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -98,87 +140,98 @@ class _MyHomePageState extends State<Login> {
                         return null;
                       },
                     ),
-                    const SizedBox(height: 25.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          String username = _usernameController.text;
-                          String password = _passwordController.text;
-                          if (username == "User") {
-                            RegExp passwordRegExp =
-                            RegExp(r'^(?=.*[a-zA-Z])(?=.*\d).{7,}$');
-                            if (passwordRegExp.hasMatch(password)) {
-                              Navigator.pushNamed(
-                                context,
-                                '/Select',
-                                arguments: username,
-                              );
-                              print("Login exitoso------------------");
-                            } else {
-                              print(
-                                  "La contraseña debe tener al menos 7 caracteres y contener letras y numeros.");
-                            }
-                          } else {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title:
-                                  const Text('Error de inicio de sesión'),
-                                  content: const Text(
-                                      'Usuario o contraseña incorrectos'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('Aceptar'),
-                                    ),
-                                  ],
-                                );
-                              },
+                  ),
+                  const SizedBox(height: 35.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        String username = _usernameController.text;
+                        String password = _passwordController.text;
+                        if (username == "User") {
+                          RegExp passwordRegExp =
+                              RegExp(r'^(?=.*[a-zA-Z])(?=.*\d).{7,}$');
+                          if (passwordRegExp.hasMatch(password)) {
+                            Navigator.pushNamed(
+                              context,
+                              '/Select',
+                              arguments: username,
                             );
+                            print("Login exitoso------------------");
+                          } else {
+                            print(
+                                "La contraseña debe tener al menos 7 caracteres y contener letras y numeros.");
                           }
+                        } else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Error de inicio de sesión'),
+                                content: const Text(
+                                  'Usuario o contraseña incorrectos',
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Aceptar'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
                         }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
                       ),
-                      child: Text(
-                        'Login',
+                    ),
+                    child: Text(
+                      'Login',
+                      style: GoogleFonts.nunitoSans(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    "He olvidado la contraseña",
+                    style: GoogleFonts.nunitoSans(
+                      color: Colors.blue,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Aún no tienes cuenta?",
                         style: GoogleFonts.nunitoSans(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Colors.white),
+                            color: Colors.black, fontSize: 16),
                       ),
-                    ),
-                    const SizedBox(height: 15.0),
-                    Text(
-                      "He olvidado la contraseña",
-                      style: GoogleFonts.nunitoSans(color: Colors.blue, fontSize: 16,),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Aún no tienes cuenta?",style: GoogleFonts.nunitoSans(),),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Text(
-                            " Crear cuenta",
-                            style: GoogleFonts.nunitoSans(color: Colors.blue,fontSize: 16,),
-                          ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          " Crear cuenta",
+                          style: GoogleFonts.nunitoSans(
+                              color: Colors.blue,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                ],
               ),
             ),
           ),
